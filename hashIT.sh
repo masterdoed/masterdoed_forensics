808d6bf2941e9f7f512bc98810733deadd169c9e
doed <doed@tuxbox.(none)>
2015-02-19 08:52:31 +0100

    update

--- hashIT.sh
+++ hashIT.sh
@@ -11,16 +11,17 @@
 
 
 ### VARS
-date=$( date "+%Y_%m_%d" )
+date=$( date "+%Y_%m_%d_hashes" )
 forensic_user=$( whoami  )
 working_path="/bin/"
-result_path="/home/$forensic_user/Desktop/doed_forensics_results/$date/"
+result_path="/home/$forensic_user/Desktop/forensics_results_$forensic_user/"
+hashes_result_path="$result_path/$date/"
 files_output="all_files.txt"
 md5_output="all_files_md5.txt"
 sha1_output="all_files_sha1.txt"
-out_files="$result_path$files_output"
-out_md5="$result_path$md5_output"
-out_sha1="$result_path$sha1_output"
+out_files="$hashes_result_path$files_output"
+out_md5="$hashes_result_path$md5_output"
+out_sha1="$hashes_result_path$sha1_output"
 
 
 echo "##################################################"
@@ -29,11 +30,12 @@ echo "##################################################"
 echo ""
 
 ### DELETING OLD_FILES
-echo "---> Deleting today's files: $result_path"
-rm -rf $result_path
+echo "---> Deleting today's files: $hashes_result_path"
+rm -rf $hashes_result_path
 
 ### CREATE RESULT PATH
 if [ ! -d $result_path ]; then mkdir $result_path; fi
+if [ ! -d $hashes_result_path ]; then mkdir $hashes_result_path; fi
 
 ### CHECK IF WORKING PATH EXISTS
 if [ ! -d $working_path ]; then echo "Path=$working_path could not be found! Please check paramaters and try again ..."; 
@@ -53,19 +55,9 @@ echo "---> Calculating SHA1 hashes >> $out_sha1"
 hashdeep -resbc sha1 -f $out_files >> $out_sha1
 
 ### FINISHED
-echo "---> All operations finished. To access your results >> cd $result_path"
+echo "---> All operations finished. To access your results >> cd $hashes_result_path"
 echo "---> Created files:"
-ls -lha $result_path
+ls -lha $hashes_result_path
 
 fi
 
-### DEBUGGING
-#echo $forensic_user
-#echo $working_path
-#echo $result_path
-#echo $files_output
-#echo $md5_output
-#echo $sha1_output
-#echo $out_files
-#echo $out_md5
-#echo $out_sha1
